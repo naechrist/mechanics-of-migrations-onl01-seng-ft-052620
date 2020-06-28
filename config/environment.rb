a6dd8afd8d80fa78e7d07d7fe733c2c43eab69be
Jenae Christensen <729jenae.christensen@gmail.com>
2020-06-28 16:01:15 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -12,16 +12,16 @@ ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
   :database => "db/artists.sqlite"
   )
-  # sql = <<-SQL
-  #   CREATE TABLE IF NOT EXISTS artists (
-  #   id INTEGER PRIMARY KEY, 
-  #   name TEXT, 
-  #   genre TEXT, 
-  #   age INTEGER,
-  #   hometown TEXT 
-  #   )
-  # SQL
-  # ActiveRecord::Base.connection.execute(sql)
+  sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS artists (
+    id INTEGER PRIMARY KEY, 
+    name TEXT, 
+    genre TEXT, 
+    age INTEGER,
+    hometown TEXT 
+    )
+  SQL
+  ActiveRecord::Base.connection.execute(sql)
 # put the code to connect to the database here
 
 require_relative "../artist.rb"
